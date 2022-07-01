c648c9ad8d0f0490605514c869b40f29ee513597
Nikolay Saitov <saitovkms@gmail.com>
2022-07-01 15:18:32 +0300

    14.4

--- app.js
+++ app.js
@@ -25,8 +25,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //   next();
 // });
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(/^(https?:\/\/(www\.)?([a-zA-z0-9-]){1,}\.?([a-zA-z0-9]){2,8}(\/?([a-zA-z0-9-])*\/?)*\/?([-._~:/?#[]@!\$&'\(\)\*\+,;=])*)/),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
 app.use(auth);
 app.use('/', require('./routes/users'));
 app.use('/', require('./routes/cards'));
