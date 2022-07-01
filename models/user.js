fa6e95247ac526d070268af4048691587292bd88
Nikolay Saitov <saitovkms@gmail.com>
2022-07-01 17:02:58 +0300

    14.8

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 // models/user.js
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -37,5 +38,22 @@ const userSchema = new mongoose.Schema({
     select: false,
   },
 });
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
+  return this.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Переданы некорректные данные'));
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Переданы некорректные данные'));
+          }
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
