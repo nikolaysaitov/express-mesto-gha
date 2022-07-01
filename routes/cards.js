ded9310d5f2955cdedcca87c07f46e0494321dcb
Nikolay Saitov <saitovkms@gmail.com>
2022-07-01 15:33:29 +0300

    14.6

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getCards,
   createCard,
@@ -8,8 +9,25 @@ const {
 } = require('../controllers/cards');
 
 router.get('/cards', getCards); // возвращает все карточки
-router.post('/cards', createCard); // создаёт карточку
-router.delete('/cards/:cardId', deleteCard); // удаляет карточку по идентификатору
-router.put('/cards/:cardId/likes', likeCard); // поставить лайк карточке
-router.delete('/cards/:cardId/likes', dislikeCard); // убрать лайк с карточки
+router.post('/cards', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().pattern(/^(https?:\/\/(www\.)?([a-zA-z0-9-]{1}[a-zA-z0-9-]*\.?)*\.{1}([a-zA-z0-9]){2,8}(\/?([a-zA-z0-9-])*\/?)*\/?([-._~:?#[]@!\$&'\(\)\*\+,;=])*)/),
+  }),
+}), createCard); // создаёт карточку
+router.delete('/cards/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), deleteCard); // удаляет карточку по идентификатору
+router.put('/cards/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), likeCard); // поставить лайк карточке
+router.delete('/cards/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), dislikeCard); // убрать лайк с карточки
 module.exports = router;
