e5e1f01d90b9483e0552f78d8b1483d5b0350017
Nikolay Saitov <saitovkms@gmail.com>
2022-07-12 15:02:41 +0300

    15.7

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { PORT = 3000 } = process.env;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { celebrate, Joi, errors } = require('celebrate');
+const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
@@ -18,6 +19,7 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 // подключаем мидлвары, роуты и всё остальное...
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(cors);
 app.use(bodyParser.urlencoded({ extended: true }));
